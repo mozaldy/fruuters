13054e6d1c8155bb3e7f4f1a8a22d02ae6430bc7
mozaldy <moxaldy@gmail.com>
2024-01-16 17:07:11 +0700

    add localization in about

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,31 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createI18n } from 'vue-i18n'
+
 import './assets/main.css'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-import { firebaseApp } from './firebase'
+const i18n = createI18n({
+  locale: 'en', // default language
+  messages: {
+    en: {
+      // English translations
+      about: 'About'
+      // Add more translations as needed
+    },
+    id: {
+      // Indonesian translations
+      about: 'Tentang'
+      // Add more translations as needed
+    }
+  }
+})
 
 const app = createApp(App)
 
 app.use(router)
+app.use(i18n) // Use the i18n instance
 
 app.mount('#app')
